396abd4ec1ae35690d47a9e1c91b6de1d0a42213
Yunfeng Wang <vra@users.noreply.github.com>
2016-04-11 20:42:58 +0800

    Update video_2_frm_ffmepg.py

--- video_2_frm_ffmepg.py
+++ video_2_frm_ffmepg.py
@@ -2,13 +2,16 @@
 FFMPEG_BIN = "D:\\program_file\\ffmpeg\\bin\\ffmpeg.exe"
 
 import subprocess as sp
-
+import os
+import glob
 
 directorys = os.listdir('D:\dataset\ucf101')
 for directory in directorys:
 	if os.path.isdir(directory):
 		files = glob.glob(directory+"/*.avi")
 		with open(file, 'rb') as infile:
-			pipe = sp.Popen([FFMPEG_BIN, '-i', '-', stdin = infile, stdout  = sp.PIPE])
+			#pipe = sp.Popen([FFMPEG_BIN, '-i', '-', stdin = infile, stdout  = sp.PIPE])
+			# USe os.system to execute shell command
+			os.system('ffmpeg -i '+ infile) 
 
 			
